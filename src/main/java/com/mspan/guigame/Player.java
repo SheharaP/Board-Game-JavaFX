28480e91fb3fa3fe46ce95c30d1584d7a6568252
Shehara Pandithasekera <mspandithasekera@gmail.com>
2022-12-28 10:09:23 +0530

    commit - test 2

--- src/main/java/com/mspan/guigame/Player.java
+++ src/main/java/com/mspan/guigame/Player.java
@@ -20,7 +20,27 @@ public class Player {
         this.joinedGame = false;
     }
 
+    public int getPlayerPosition() {
+        return this.position;
+    }
 
+    public void setPlayerPosition(int val) {
+        this.position += val;
+    }
+
+    public void setChangedPosition(int val) {
+        this.position = val;
+    }
+    public String getColor() {
+        return this.color;
+    }
+    public boolean joinGame() {
+        return this.joinedGame;
+    }
+
+    public void setJoinedGame() {
+        this.joinedGame = true;
+    }
 
 
 
